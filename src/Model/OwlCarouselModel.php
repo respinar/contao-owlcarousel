0b36a9be17d5fc2cabc1dbfb60fbd6e4de9eb438
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-18 01:37:36 +0330

    update namespace

--- src/Model/OwlCarouselModel.php
+++ src/Model/OwlCarouselModel.php
@@ -3,22 +3,25 @@
 /**
  * Owlcarousel Extension for Contao Open Source CMS
  *
- * @copyright  Copyright (c) 2017, Respinar
- * @author     Respinar <info@respinar.com>
- * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
- * @link       https://respinar.com/
+ * @copyright  Copyright (c) 2023, Respinar
+ * 
+ * @author     Hamid Peywasti <hamid@respinar.com>
+ * 
+ * @license    MIT 
  */
 
 
 /**
  * Namespace
  */
-namespace Respinar\OwlCarousel\Model;
+namespace Respinar\OwlCarouselBundle\Model;
+
+use Contao\Model;
 
 /**
  * Class OwlCarouselModel
  */
-class OwlCarouselModel extends \Model
+class OwlCarouselModel extends Model
 {
 
 	/**
