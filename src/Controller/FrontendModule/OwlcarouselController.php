17951cfd3b8e906199a92133924d5706d3c54597
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-08-18 15:22:56 +0330

    Create new frontend module

--- src/Controller/FrontendModule/OwlcarouselController.php
+++ src/Controller/FrontendModule/OwlcarouselController.php
@@ -18,12 +18,47 @@ use Contao\ModuleModel;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Respinar\OwlcarouselBundle\Helper\Owlcarousel;
+use Respinar\OwlcarouselBundle\Model\OwlcarouselModel;
+use Respinar\OwlcarouselBundle\Model\OwlcarouselSlideModel;
 
 #[AsFrontendModule(category: 'application')]
 class OwlcarouselController extends AbstractFrontendModuleController
 {
     protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
     {
+
+        $intTotal = OwlcarouselSlideModel::countPublishedByPid($model->owl_carousel);
+
+		if ($intTotal < 1)
+		{
+			$template->empty = $GLOBALS['TL_LANG']['MSC']['owlcarousel_noSlide'];
+
+			return $template->getResponse();
+		}
+
+		$objOwlCarousel = OwlcarouselModel::findBy('id',$model->owl_carousel);
+		$template->setData($objOwlCarousel->row());
+
+
+		$objSlides = OwlcarouselSlideModel::findPublishedByPid($model->owl_carousel);
+
+		// Items found
+		if ($objSlides !== null)
+		{
+
+			$template->slides = Owlcarousel::parseSlides($objSlides, $model);
+		}
+
+		$GLOBALS['TL_BODY'][] = Template::generateScriptTag('bundles/respinarowlcarousel/OwlCarousel2/owl.carousel.min.js', false, null);
+        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/OwlCarousel2/assets/owl.carousel.min.css', false, null);
+        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/OwlCarousel2/assets/owl.theme.default.min.css', false, null);
+
+		if ($objOwlCarousel->owl_animateIn || $objOwlCarousel->owl_animateOut) {
+			$GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/OwlCarousel2/animate/animate.min.css', false, null);
+		}
+
         return $template->getResponse();
+
     }
 }
