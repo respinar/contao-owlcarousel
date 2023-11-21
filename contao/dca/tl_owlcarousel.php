e24bdece42c080dd21aed1f08c10f01d5735650f
Hamid Peywasti <hamid@respinar.com>
2023-11-21 09:14:57 +0330

    fix: Undefined array key "deleteConfirm"

--- contao/dca/tl_owlcarousel.php
+++ contao/dca/tl_owlcarousel.php
@@ -74,7 +74,7 @@ $GLOBALS['TL_DCA']['tl_owlcarousel'] = array
 			(
 				'href'     => 'act=delete',
 				'icon'     => 'delete.svg',
-				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+				'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
 			),
 			'show' => array
 			(
