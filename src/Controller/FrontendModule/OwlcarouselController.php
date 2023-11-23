3fc1c54217527de67b6b6a2e8afc166ff7d61528
Hamid Peywasti <hamid@respinar.com>
2023-11-23 15:48:27 +0330

    update assets path

--- src/Controller/FrontendModule/OwlcarouselController.php
+++ src/Controller/FrontendModule/OwlcarouselController.php
@@ -52,12 +52,12 @@ class OwlcarouselController extends AbstractFrontendModuleController
 			$template->slides = Owlcarousel::parseSlides($objSlides, $model);
 		}
 
-		$GLOBALS['TL_BODY'][] = Template::generateScriptTag('bundles/respinarowlcarousel/OwlCarousel2/owl.carousel.min.js', false, null);
-        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/OwlCarousel2/assets/owl.carousel.min.css', false, null);
-        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/OwlCarousel2/assets/owl.theme.default.min.css', false, null);
+		$GLOBALS['TL_BODY'][] = Template::generateScriptTag('bundles/respinarowlcarousel/owl.carousel.min.js', false, null);
+        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/assets/owl.carousel.min.css', false, null);
+        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/assets/owl.theme.default.min.css', false, null);
 
 		if ($objOwlCarousel->animateIn || $objOwlCarousel->animateOut) {
-			$GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/OwlCarousel2/animate/animate.min.css', false, null);
+			$GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/animate/animate.min.css', false, null);
 		}
 
         return $template->getResponse();
