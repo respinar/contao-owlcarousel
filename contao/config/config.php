d255de64a74439e927803e39519a1be2f80a16c4
Hamid Peywasti <hamid@respinar.com>
2023-12-13 00:25:57 +0330

    change namespaces and cleanup

--- contao/config/config.php
+++ contao/config/config.php
@@ -8,29 +8,13 @@
  * @license MIT
  */
 
-use Respinar\OwlcarouselBundle\Model\OwlcarouselModel;
-use Respinar\OwlcarouselBundle\Model\OwlcarouselSlideModel;
+use Respinar\OwlCarouselBundle\Model\OwlCarouselModel;
+use Respinar\OwlCarouselBundle\Model\OwlCarouselSlideModel;
 
- array_insert($GLOBALS['BE_MOD']['content'], 1, array
-(
-	'owlcarousel' => array
-	(
-		'tables' => array('tl_owlcarousel', 'tl_content')
-	)
-));
+$GLOBALS['BE_MOD']['content'][] = ['owlcarousel' => array('tables' => array('tl_owlcarousel', 'tl_content'))];
 
 /**
  * Register models
  */
- $GLOBALS['TL_MODELS']['tl_owlcarousel']       = OwlcarouselModel::class;
- $GLOBALS['TL_MODELS']['tl_owlcarousel_slide'] = OwlcarouselSlideModel::class;
-
-/**
- * Front end modules
- */
-//$GLOBALS['FE_MOD']['application']['owlcarousel']   = 'Respinar\OwlCarousel\Frontend\Module\ModuleOwlCarousel';
-
-/**
- * Content elements
- */
-//$GLOBALS['TL_CTE']['miscellaneous']['owlcarousel'] = 'Respinar\OwlCarousel\Frontend\Element\ContentOwlCarousel';
+$GLOBALS['TL_MODELS']['tl_owlcarousel']       = OwlCarouselModel::class;
+$GLOBALS['TL_MODELS']['tl_owlcarousel_slide'] = OwlCarouselSlideModel::class;
