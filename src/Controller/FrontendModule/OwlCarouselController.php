9947e56d3465cd37a1df1a2733f8ef4219157274
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-01-03 18:47:13 +0330

    use OwlCarouselRenderer fort redering

--- src/Controller/FrontendModule/OwlCarouselController.php
+++ src/Controller/FrontendModule/OwlCarouselController.php
@@ -16,51 +16,21 @@ use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\ModuleModel;
 use Contao\Template;
-use Contao\StringUtil;
+use Respinar\OwlCarouselBundle\OwlCarouselRenderer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Respinar\OwlCarouselBundle\OwlCarousel;
-use Respinar\OwlCarouselBundle\Model\OwlcarouselModel;
-use Respinar\OwlCarouselBundle\Model\OwlcarouselSlideModel;
 
-#[AsFrontendModule(category: 'application', template: 'mod_owlcarousel')]
+#[AsFrontendModule('owlcarousel', category: 'application', template: 'mod_owlcarousel')]
 class OwlCarouselController extends AbstractFrontendModuleController
 {
+    public function __construct(private readonly OwlCarouselRenderer $renderer)
+    {
+    }
+
     protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
     {
-
-        $intTotal = OwlcarouselSlideModel::countPublishedByPid($model->owl_carousel);
-
-		if ($intTotal < 1)
-		{
-			$template->empty = $GLOBALS['TL_LANG']['MSC']['owlcarousel_noSlide'];
-
-			return $template->getResponse();
-		}
-
-		$objOwlCarousel = OwlcarouselModel::findBy('id',$model->owl_carousel);
-		$template->setData($objOwlCarousel->row());
-
-		$template->navText = json_encode(StringUtil::deserialize($objOwlCarousel->navText));
-
-		$objSlides = OwlcarouselSlideModel::findPublishedByPid($model->owl_carousel);
-
-		// Items found
-		if ($objSlides !== null)
-		{
-
-			$template->slides = Owlcarousel::parseSlides($objSlides, $model);
-		}
-
-		$GLOBALS['TL_BODY'][] = Template::generateScriptTag('bundles/respinarowlcarousel/owl.carousel.min.js', false, null);
-        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/assets/owl.carousel.min.css', false, null);
-        $GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/assets/owl.theme.default.min.css', false, null);
-
-		if ($objOwlCarousel->animateIn || $objOwlCarousel->animateOut) {
-			$GLOBALS['TL_BODY'][] = Template::generateStyleTag('bundles/respinarowlcarousel/animate/animate.min.css', false, null);
-		}
+        $this->renderer->render($template, $model, 'mod-owlcarousel');
 
         return $template->getResponse();
-
     }
 }
